0e3e728c85e6d7baee6f8d0319eb83974aeb84b9
Rudy Lattae <rudylattae@gmail.com>
2014-01-19 04:03:18 -0600

    Specs for initialization and #set / #get for strings

--- docs/spec/epoSpec.js
+++ docs/spec/epoSpec.js
@@ -1,5 +1,49 @@
 describe('epo', function() {
-    it('is beautiful', function() {
-        expect( true ).toBe( true );
+
+    function localStoreageHasData( key ) {
+        return localStorage.getItem( key ) !== undefined;
+    }
+
+
+    describe('creating', function() {
+        var ls;
+
+        it('initializes with namespace', function() {
+            ls = epo('mystash');
+
+            expect( ls ).not.toBeNull();
+        });
+
+        it('initializes without namespace', function() {
+            ls = epo();
+
+            expect( ls ).not.toBeNull();
+        });
+    });
+
+    describe('when handling strings', function() {
+        var ls, store;
+
+        beforeEach(function() {
+            ls = epo('strings');
+        });
+
+        it('persists and retrieves simple string', function() {
+            var key = 'simple',
+                value = 'Simple String';
+
+            ls.set(key, value);
+
+            expect( ls.get(key) ).toEqual( value );
+        });
+
+        it('persists and retrieves string with crazy characters', function() {
+            var key = 'complex',
+                value = 'Some overly c\'omplex string/ with "quoted" and *strred* and & etc...';
+
+            ls.set(key, value);
+
+            expect( ls.get(key) ).toEqual( value );
+        });
     });
 });
--- epo.js
+++ epo.js
@@ -2,9 +2,24 @@
     // imports (sor to f)
     var storageAdapter = g.localStorage;
 
-    function epo( namespace ) {
 
+    function epo( namespace ) {
+        return new Epo( namespace );
     }
+
+
+    function Epo( namespace ) {
+        this._ns = namespace;
+    }
+
+    Epo.prototype.set = function set( key, value ) {
+        storageAdapter.setItem( key, value );
+    };
+
+    Epo.prototype.get = function get( key ) {
+        return storageAdapter.getItem( key );
+    };
+
 
     // exports
     g.epo = epo;
