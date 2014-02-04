dd795ca36dcea3268a82247987e480e8ebb9af53
Rudy Lattae <rudylattae@gmail.com>
2014-02-03 21:58:43 -0600

    Packaging underway

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    pkg = require('./package.json');
 
 
-gulp.task('default', function() {
+gulp.task('package', function() {
+    return gulp.src('tote.js')
+        .pipe(concat(pkg.name + '.js'))
+        .pipe(gulp.dest('./www/dist'))
+        .pipe(rename(pkg.name + '.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./www/dist'));
+});
+
+gulp.task('default', ['package'], function() {
 
 });
